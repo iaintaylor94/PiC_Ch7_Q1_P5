f57f5f1ee0f9fbcec7ada058f73fd8496fd36e54
Iain Taylor <iain.taylor.94@gmail.com>
2023-07-17 13:03:03 +0000

    completed program

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+
+  int array_values[10] = { 0, 1, 4, 9, 16 }; // Initialize array_values[0] - array_values[4]
+  int i;
+
+  // Initialize array_values[5] - array_values[9]
+  for (i = 5; i < 10; i++)
+    array_values[i] = i * i;
+
+  // Print the contents of array_values[0] - array_values[9]
+  for ( int i = 0; i < 10; i++ ) 
+    printf ("array_values[%i] = %i\n", i, array_values[i]);
+  
   return 0;
 }
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Programming in C
+## Chapter 7
+### Question 1: Part 5
+
+This problem is from chapter 7 of the textbook, it is to implement program 7.5[^1] from the textbook.
+
+You may wish to run the program and compare its output.
+
+[^1]: Programming in C (Third edition) By Stephen G. Kochan - Page 107 - Program 7.5
